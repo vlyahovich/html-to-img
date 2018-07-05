22c0f69afe22c611f0f77504d05fe704e3e52e5c
Vadim Liakhovich <v.liakhovich@yandex.ru>
2018-07-05 18:57:11 +0300

    configurable port

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
-const logger = require('koa-logger')
+const logger = require('koa-logger');
 
 const createStaticMiddleware = require('./middlewares/koaStaticMiddleware');
 const createSnapshotRoute = require('./middlewares/koaSnapshoutMiddleware');
+
+const PORT = process.env.PORT || 3256;
 
 // logger
 app.use(logger())
@@ -14,4 +16,4 @@ app.use(createStaticMiddleware());
 // routes
 app.use(createSnapshotRoute());
  
-app.listen(3000);
+app.listen(PORT);
